76d1f6708ae50a0d6e77c3efc4ec2c9a430d9717
khuzan <khuzan557@gmail.com>
2025-01-07 15:23:40 +0800

    fix deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-   alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
-  }
-})
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//    alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}]
+//   }
+// })
+
+export default defineConfig({ base: "/{fitness-gym}", plugins: [react()], }) 
